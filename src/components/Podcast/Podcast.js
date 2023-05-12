b08b316afe81d2b64e8da0334cf2fe7a4ebdf22e
masudpro121 <masud.pro.121@gmail.com>
2023-05-12 09:57:22 +0600

    podcast page design done

--- src/components/Podcast/Podcast.js
+++ src/components/Podcast/Podcast.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./podcast.css";
 
-export default function Podcast({data}) {
+export default function Podcast({ data }) {
+ 
   return (
-    <Link to={"/podcast/"+data.id}>
-        <img style={{width:'100px', height:'100px'}} src={data.image} alt="" />
-        <h3>{data.title}</h3>
-        <p>{data.description}</p>
-    </Link>
-  )
+    <div className="podcast">
+      <div className="item">
+        <div>
+          <img
+            style={{ width: "100px", height: "100px" }}
+            src={data.image}
+            alt=""
+          />
+        </div>
+        <div>
+          <Link to={"/podcast/" + data.id}>
+            <h5>{data.title}</h5>
+            <p className="mt-2">{data.description.slice(0, 250)}...</p>
+          </Link>
+          <div className="categories">
+           {
+            Object.values(data.categories).map((category, id)=>{
+              return (
+                <small key={category+id}>#{category}</small>
+              )
+            })
+           }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
